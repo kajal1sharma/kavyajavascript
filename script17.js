68d7d551263952889b43461729034f0a8080b080
kajal <kajal@gmail.com>
2024-04-26 20:25:02 +0530

    ..

--- script17.js
+++ script17.js
@@ -31,31 +31,51 @@
 // * * * * 
 // * * * * *
 
-let num =Number(prompt("HOw many stars in a line"));
-str="";
-for(let i =0 ; i<num;i++){
-   //row
+// let num =Number(prompt("HOw many stars in a line"));
+// str="";
+// for(let i =0 ; i<num;i++){
+//    //row
 
-   //stars add 
-    for(let j =0;j<=i;j++){
+//    //stars add 
+//     for(let j =0;j<=i;j++){
+//         str =str+"* ";
+//     }
+
+//     str=str+"\n";
+
+// }
+
+
+// console.log(str)
+
+
+
+
+// # # # # * // 4 hash 1 star
+// # # # * * //3 hash 2 star
+// # # * * *  2hash 3 star
+// # * * * *  1hash 4star 
+// * * * * *  0hash 5star
+
+let row =5;
+//number of rows
+let str ="";
+for(let i=0;i<row;i++){
+    //hash
+    for(let j=row-1;j>i ;j--){ //4 3 2
+        str= str+"  ";
+    }
+    //str="# # # # * \n# # #"
+    for(let j=0 ;j<=i;j++){
         str =str+"* ";
     }
-
-    str=str+"\n";
-
+    //str = "# # # # * \n# # # * * "
+    str =str +"\n";
+    //str ="# # # # * \n# # # * * \n"
 }
-
 
 console.log(str)
 
-
-
-
-//         * 
-//       * *
-//     * * *
-//   * * * * 
-// * * * * *
 
 
 // * * * * *
