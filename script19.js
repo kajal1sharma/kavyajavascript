447e493319044539263c66f2f9201bec2b7b296d
kajal <kajal@gmail.com>
2024-04-26 20:44:12 +0530

    ..

---
+++ script19.js
@@ -0,0 +1,47 @@
+//let str= "my dog name is tyson"
+
+
+//let arr =["teena", "meena", "priya","raju"]
+
+
+// for(let key in arr){
+//     console.log(key+"="+arr[key])
+// }
+
+// {
+//     "0":"teena",
+//     "1":"meena",
+//     "2":"priya",
+//     "3":"raju",
+//     length:4
+// }
+
+
+// let obj ={name:"teena", age:34, phone:"234234234"}
+
+// console.log(obj.age)
+// console.log(obj["name"])
+
+
+// for(let key in obj){
+//     let str = key +" = "+obj[key];
+//     console.log(str)
+//     // console.log(key)
+// }
+
+
+// strarr = str.split(" ");
+// console.log(strarr)
+
+
+// for(let ch of str){
+//     let dh =ch+"0";
+//     console.log(dh)
+// }
+
+
+
+// for(let i=0;i<str.length;i++){
+//     let ch = str.charAt(i)
+//     console.log(ch)
+// }
