64a035f111497533af3d96d4e85e08f66f6e823d
kajal <kajal@gmail.com>
2024-05-04 12:01:57 +0530

    ..

--- script21.js
+++ script21.js
@@ -1,32 +1,101 @@
 
 window.addEventListener('load',()=>{
+ 
+    let enterbutton = document.getElementById('enterbutton')
+    let useridInp =  document.getElementById('userid')
+    let input =1;
+    
+    useridInp.addEventListener('input',(event )=>{
+        input = Number(event.target.value);
+        // console.log(input)
+    })
+
+    enterbutton.addEventListener('click',async ()=>{
+        let result = await fetch("https://dummyjson.com/todos")
+        result = await result.json();
+        console.log(result.todos )
+
+        let filteredResult = result.todos.filter((obj)=>{
+            if(obj.userId===input){
+                return true;
+            }
+        })
+
+        // printing
+        filteredResult.forEach(obj => {
+            let text = obj.todo
+            let span  = document.createElement('span')
+            if(obj.completed===true){
+                span.textContent="completed"
+                span.style.color="green"
+            }
+            else{
+                span.textContent="pending"
+                span.style.color="red"
+            }
+            let li = document.createElement('li')
+            
+            li.textContent=text;
+            li.appendChild(span)
+            list.appendChild(li)
+
+        });
+
+    })
+
+
+
+
     let load = document.getElementById("loadButton")
     let list = document.getElementById("list")
     load.addEventListener('click',async ()=>{
-        let result = await fetch("https://dummyjson.com/todos")
+       let result = await fetch("https://dummyjson.com/todos")
         result = await result.json();
-        console.log(result )
+        console.log(result.todos )
 
-        for(let i =0;i< result["todos"].length; i++){
-            let text = result.todos[i].todo;
-             console.log(text)
-            let li = document.createElement('li')
-            let span = document.createElement('span')
-            if(result.todos[i].completed===true){
-                span.innerText="completed"
-                span.style.backgroundColor="green"
+
+
+        result.todos.forEach(obj => {
+            let text = obj.todo
+            let span  = document.createElement('span')
+            if(obj.completed===true){
+                span.textContent="completed"
+                span.style.color="green"
             }
             else{
-                span.innerText="pending"
-                span.style.backgroundColor="red"
+                span.textContent="pending"
+                span.style.color="red"
             }
+            let li = document.createElement('li')
             
-            li.textContent = text;
+            li.textContent=text;
             li.appendChild(span)
-            
-            list.appendChild(li);
+            list.appendChild(li)
 
-        }
+        });
+
+
+
+        // for(let i =0;i< result["todos"].length; i++){
+        //     let text = result.todos[i].todo;
+        //      console.log(text)
+        //     let li = document.createElement('li')
+        //     let span = document.createElement('span')
+        //     if(result.todos[i].completed===true){
+        //         span.innerText="completed"
+        //         span.style.backgroundColor="green"
+        //     }
+        //     else{
+        //         span.innerText="pending"
+        //         span.style.backgroundColor="red"
+        //     }
+            
+        //     li.textContent = text;
+        //     li.appendChild(span)
+            
+        //     list.appendChild(li);
+
+        // }
 
     })
 
--- script22.js
+++ script22.js
@@ -1,9 +1,15 @@
  alert("hello world");
 
- let h1= document.getElementsByTagName('h1')
- console.log(h1[0])
 
- h1[0].addEventListener('click',()=>{
-    h1[0].style.color="red"
- })
+//  let para= document.querySelector("p")
+let paras = document.getElementsByTagName('p')
+ console.log(paras)
+ paras[0].style.color="blue"
+
+//  let h1= document.getElementsByTagName('h1')
+//  console.log(h1[0])
+
+//  h1[0].addEventListener('click',()=>{
+//     h1[0].style.color="red"
+//  })
 
