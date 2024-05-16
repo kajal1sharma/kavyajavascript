e047b36fd1e554f3587b8568a327e8767b8bcd5d
kajal <kajal@gmail.com>
2024-05-16 14:59:38 +0530

    ..

--- scripttodo.js
+++ scripttodo.js
@@ -13,17 +13,41 @@ btn.addEventListener('click',()=>{
     if(str.length !==0 && str!==null && str!==undefined){
     let listitem = document.createElement('li');
 
-    listitem.innerText=str;
+    let inputbox  = document.createElement('input')
+    inputbox.value=str
+    inputbox.style.border ="none"
+    inputbox.disabled = true;
+    listitem.appendChild(inputbox)
     let removeBtn = document.createElement('button')
     removeBtn.innerText = "X";
+    let editBtn = document.createElement('button')
+    editBtn.innerText = "edit";
     listitem.appendChild(removeBtn)
+    listitem.appendChild(editBtn)
     list.appendChild(listitem)
 
-    
+
     removeBtn.addEventListener('click',()=>{
         list.removeChild(listitem)
     })
 
+    editBtn.addEventListener('click',()=>{
+
+        inputbox.disabled=false;
+        inputbox.style.border="1px solid black"
+        inputbox.focus=true;
+
+        inputbox.addEventListener('input',(event)=>{
+            let size =event.target.value.length;
+                if(event.target.value.charAt(size-1)==="p")
+                    {
+                        inputbox.value=event.target.value
+                        inputbox.style.border="none";
+                        inputbox.disabled=true;
+                    }
+        }) 
+
+    })
     
     }
 })
